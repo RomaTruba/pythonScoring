0517018624918704cf4880564a4221af0649c214
tramwai <truba_roma@mail.ru>
2025-05-10 17:19:02 +0300

    file

---
+++ src/login.py
@@ -0,0 +1,25 @@
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton
+
+
+class LoginDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Авторизация")
+        self.setFixedSize(300, 200)
+
+        layout = QVBoxLayout()
+
+        self.login_input = QLineEdit()
+        self.login_input.setPlaceholderText("Логин")
+        layout.addWidget(self.login_input)
+
+        self.password_input = QLineEdit()
+        self.password_input.setPlaceholderText("Пароль")
+        self.password_input.setEchoMode(QLineEdit.Password)
+        layout.addWidget(self.password_input)
+
+        login_button = QPushButton("Войти")
+        login_button.clicked.connect(self.accept)
+        layout.addWidget(login_button)
+
+        self.setLayout(layout)
