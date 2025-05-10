0bb5e053c9c8497e65d99d9867976525e8827105
tramwai <truba_roma@mail.ru>
2025-05-10 16:48:22 +0300

    Add file

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from src.app import CreditScoringApp
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    window = CreditScoringApp()
+    window.show()
+    sys.exit(app.exec_())
