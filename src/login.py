021c3b8e004482f8a856d09f2a93fde9c36f9565
tramwai <truba_roma@mail.ru>
2025-05-30 15:07:23 +0300

    file

--- src/login.py
+++ src/login.py
@@ -1,5 +1,4 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton
-
+from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QLineEdit, QPushButton)
 
 class LoginDialog(QDialog):
     def __init__(self):
